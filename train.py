1652a0e47f2d8c1065337a29c6668e6676139577
Aaron Berk (grimpoteuthis) <aberk@math.ubc.ca>
2020-06-26 16:10:11 -0700

    Hopefully informative error information when lack of convergence

--- train.py
+++ train.py
@@ -29,7 +29,11 @@ def get_default_autoencoder_loss(lamda=None):
     def loss_fn(x_recon, x_true, mu, log_var):
         try:
             BCE = F.binary_cross_entropy(x_recon, x_true, reduction="sum")
-        except RuntimeError:
+        except RuntimeError as rte:
+            import sys
+
+            print(rte.args[0])
+            print(sys.exc_info())
             pdb.set_trace()
         KLD = -0.5 * torch.sum(1 + log_var - mu.pow(2) - log_var.exp())
         return BCE + lamda * KLD
